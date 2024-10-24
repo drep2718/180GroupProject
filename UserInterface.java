bef45d5d7aa099ac23616d2089275901ef837663
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-10-23 20:48:38 -0400

    Create UserInterface.java
    Added the needed method headers into the interface

---
+++ UserInterface.java
@@ -0,0 +1,18 @@
+public interface UserInterface {
+
+     String getUsername();
+     void setUsername (String username);
+     Boolean checkUsernameAvailability(String username);
+     String getPassword();
+     void setPassword(String password);
+     Boolean validatePassword(String username, String password);
+     String getBio();
+     void setBio(String bio);
+     String createProfile(String username, String password, String bio);
+     void updateUsername(String newUsername);
+     void updatePassword(String newPassword);
+     void updateBio(String newBio);
+     String removeProfile(String username, String password);
+     String findProfile(String username);
+
+}
