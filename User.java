c47ccef843bf7831f2419d12501d1ff1d7512266
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-10-23 20:51:38 -0400

    Update User.java
    I added the implements UserInterface
    I added the plan constructor to initialize all of our instance variables
    I added the 3 update methods

--- User.java
+++ User.java
@@ -1,12 +1,34 @@
 import java.util.ArrayList;
 
-public class User {
+public class User implements UserInterface {
     private ArrayList<String> usernames;
     String username;
     private ArrayList<String> passwords;
     String password;
     private ArrayList<String> bios;
     String bio;
+
+    public User(ArrayList usernames, String username, ArrayList passwords,
+                String password, ArrayList bios, String bio) {
+        this.username = username;
+        this.usernames = usernames;
+        this.passwords = passwords;
+        this.password = password;
+        this.bio = bio;
+        this.bios = bios;
+    }
+
+     public void updateUsername(String newUsername) {
+        this.username = newUsername;
+    }
+
+    public void updateBio(String newBio) {
+        this.bio = newBio;
+    }
+
+    public void updatePassword(String newPassword) {
+        this.password = newPassword;
+    }
 
 
 
