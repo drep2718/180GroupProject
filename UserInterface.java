bbb8578e8839bee07e9e281bad6f0194cd315127
Linh Dang <dang92@purdue.edu>
2024-10-24 15:09:49 -0400

    Update UserInterface.java
    fixed 'Boolean' to 'boolean'

--- UserInterface.java
+++ UserInterface.java
@@ -1,18 +1,18 @@
 public interface UserInterface {
 
-     String getUsername();
-     void setUsername (String username);
-     Boolean usernameAvali(String username);
-     String getPassword();
-     void setPassword(String password);
-     Boolean validatePassword(String username, String password);
-     String getBio();
-     void setBio(String bio);
-     void createProfile(String username, String password, String bio);
-     void updateUsername(String newUsername);
-     void updatePassword(String newPassword);
-     void updateBio(String newBio);
-     String removeProfile(String username, String password);
-     String findProfile(String username);
+    String getUsername();
+    void setUsername (String username);
+    boolean checkUsernameAvailability(String username);
+    String getPassword();
+    void setPassword(String password);
+    boolean validatePassword(String username, String password);
+    String getBio();
+    void setBio(String bio);
+    void createProfile(String username, String password, String bio);
+    void updateUsername(String newUsername);
+    void updatePassword(String newPassword);
+    void updateBio(String newBio);
+    String removeProfile(String username, String password);
+    String findProfile(String username);
 
 }
