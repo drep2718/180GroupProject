2ef0244f1b5216689ec4dccb2525396fc584af12
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-10-23 20:05:07 -0400

    Update User.java
    Added instance variables

--- User.java
+++ User.java
@@ -1,1 +1,14 @@
+import java.util.ArrayList;
+
+public class User {
+    private ArrayList<String> usernames;
+    String username;
+    private ArrayList<String> passwords;
+    String password;
+    private ArrayList<String> bios;
+    String bio;
+
+
+
+}
 
