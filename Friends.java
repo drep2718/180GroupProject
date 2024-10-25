95369cdb66ea6533c029cecd30431ea925abf77c
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-10-24 21:17:44 -0400

    Update Friends.java
    added the instance variables and interface implementation

--- Friends.java
+++ Friends.java
@@ -1,1 +1,9 @@
+import java.util.ArrayList;
+
+public class Friends implements FriendsInterface{
+    public User user;
+    ArrayList<String> friendsList;
+    ArrayList<String> blockedList;
+
+}
 
