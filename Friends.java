0cecb2d5838dcc7b180cf2df24463decd869ab7a
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-02 13:53:37 -0400

    Update Friends.java
    made instance variables private

--- Friends.java
+++ Friends.java
@@ -1,9 +1,9 @@
 import java.util.ArrayList;
 
 public class Friends implements FriendsInterface{
-    public User user;
-    ArrayList<String> friendsList;
-    ArrayList<String> blockedList;
+    private User user;
+    private ArrayList<String> friendsList;
+    private ArrayList<String> blockedList;
 
     public void removeFriend(User userToRemove) {
         if (friendsList.contains(userToRemove)) {
