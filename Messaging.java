7c2b840e7196a6e3424a78d24b70aea885d9f6f6
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-02 13:53:09 -0400

    Update Messaging.java
    added instance variables

--- Messaging.java
+++ Messaging.java
@@ -1,1 +1,11 @@
 
+public class Messaging implements MessagingInterface{
+    private User sender;
+    private User receiver;
+    private String content;
+    private String date;
+    private Boolean isRead;
+    private Arraylist<Strings> messageHistory;
+
+}
+  
