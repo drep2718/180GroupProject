4f73821b7e3c8c7d4b51205d522415ddb358458b
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-02 21:09:39 -0400

    Update MessagingInterface.java

--- MessagingFolder/MessagingInterface.java
+++ MessagingFolder/MessagingInterface.java
@@ -5,6 +5,11 @@ public interface MessagingInterface {
     boolean isRead();
     void Report (User sender, User receiver);
     void deleteConversation (User sender, User receiver);
+    String toString();
+    void saveToFile(Messaging messages); // make sure each User has a separate .txt file for all their messages
+    void rewriteMessages();
+
+
 
 }
 
