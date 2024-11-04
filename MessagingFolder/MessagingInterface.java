b17ddd6a3ab91f51b9d4f4f143987a4a005afd39
Santhosh Vaddi <91930012+santhoshv25@users.noreply.github.com>
2024-11-03 22:23:50 -0500

    Update MessagingInterface.java

--- MessagingFolder/MessagingInterface.java
+++ MessagingFolder/MessagingInterface.java
@@ -6,7 +6,6 @@ public interface MessagingInterface {
     void report (User sender, User receiver, Messaging content);
     void deleteConversation (User sender, User receiver);
     String toString();
-    void saveToFile(Messaging messages); // make sure each User has a separate .txt file for all their messages
+    void saveToFile(); // make sure each User has a separate .txt file for all their messages
     void rewriteMessages();
-
 }
