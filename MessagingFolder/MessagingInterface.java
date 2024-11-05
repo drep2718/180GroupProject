5bb18bf67cd62ad178cadeb52e76e6b36966d79a
Santhosh Vaddi <91930012+santhoshv25@users.noreply.github.com>
2024-11-04 22:04:20 -0500

    Update MessagingInterface.java

--- MessagingFolder/MessagingInterface.java
+++ MessagingFolder/MessagingInterface.java
@@ -1,18 +1,49 @@
+import java.util.ArrayList;
+
 public interface MessagingInterface {
+    String getMessageType();
 
-    void sendMessage(Friends sender, User receiver, Messaging content);
+    void setMessageType(String messageType);
 
-    void deleteMessage(Friends sender, User receiver, Messaging content);
+    ArrayList<Messaging> getMessageHistory();
 
-    boolean isRead();
+    void setMessageHistory(ArrayList<Messaging> messageHistory);
 
-    void report(User sender, User receiver, Messaging content);
-
-    void deleteConversation(User sender, User receiver);
+    void rewriteMessages();
 
     String toString();
 
-    void saveToFile(); // make sure each User has a separate .txt file for all their messages
+    User getSender();
 
-    void rewriteMessages();
+    Friends getReceiver();
+
+    String getContent();
+
+    String getDate();
+
+    boolean isRead();
+
+    Boolean getIsRead();
+
+    void saveToFile();
+
+    void sendMessage(User sender, Friends receiver, String content, String date, Boolean isRead);
+
+    void sendAllFriendsMessage(User sender, String content, String date, Boolean isRead);
+
+    void sendAllUsersMessage(User sender, String content, String date, Boolean isRead);
+
+    void deleteMessage(Friends sender, User receiver, String content, String date, Boolean isRead);
+
+    void deleteFriendsPhotoMessage(User sender, String content, String date, Boolean isRead);
+
+    void deleteAllPhotoMessage(User sender, String content, String date, Boolean isRead);
+
+    void report(User sender, String content);
+
+    void deleteConversation(User userOne, User userTwo);
+
+    void deleteAllFriendsConversation(User userOne);
+
+    void deleteAllUsersConversation(User userOne);
 }
