be591a1f80de9e6afe6b7eb6de6dc3b2628a00a9
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-04 19:04:12 -0500

    Update  Messaging.java

--- MessagingFolder/ Messaging.java
+++ MessagingFolder/ Messaging.java
@@ -2,12 +2,30 @@ import java.util.ArrayList;
 import java.io.*;
 
 public class Messaging implements MessagingInterface {
-    private Friends sender;
-    private User receiver;
+    private User sender;
+    private Friends receiver;
     private String content;
     private String date;
     private Boolean isRead;
     private ArrayList<Messaging> messageHistory = new ArrayList<>();
+    private String messageType;
+
+    public Messaging(User sender, Friends receiver, String content, String date, Boolean isRead) {
+        this.sender = sender;
+        this.receiver = receiver;
+        this.content = content;
+        this.date = "";
+        this.isRead = false;
+        this.messageType = Single;
+    }
+
+    public Messaging(User sender, String content, ArrayList<User> users, String date, Boolean isRead) {
+        this.sender = sender;
+        this.content = content;
+        this.date = "";
+        this.isRead = false;
+    }
+
 
     @Override
     public ArrayList<Messaging> getMessageHistory() {
@@ -20,12 +38,16 @@ public class Messaging implements MessagingInterface {
     }
 
     public void rewriteMessages() {
+        String senderFile;
         for (Messaging m : messageHistory) {
             String[] histSplit = m.toString().split(":");
-            String senderFile = histSplit[0] + ".txt";
+            if (histSplit.length == 2) {
+                 senderFile = histSplit[0] + ".txt";
+            }if (histSplit.length == 3) {
+                 senderFile = histSplit[0] + "AllFriends.txt";
+            }
 
-            try (BufferedWriter bfw =
-                         new BufferedWriter(new FileWriter(senderFile, false))) {
+            try (BufferedWriter bfw = new BufferedWriter(new FileWriter(senderFile, false))) {
                 for (int i = 0; i < messageHistory.size(); i++) {
                     Messaging message = messageHistory.get(i);
                     bfw.write(message.toString());
@@ -38,22 +60,19 @@ public class Messaging implements MessagingInterface {
     }
 
     public String toString() {
-        return sender.getUser().getUsername() + ":" + content + ":" + receiver.getUsername();
+        if (receiver != null) {
+            return sender.getUsername() + ":" + content + ":" + receiver.getUser().getUsername();
+        } else {
+            return sender.getUsername() + ":" + content;
+        }
     }
 
-    public Messaging(Friends sender, User receiver, String content, String date, Boolean isRead) {
-        this.sender = sender;
-        this.receiver = receiver;
-        this.content = content;
-        this.date = "";
-        this.isRead = false;
-    }
 
-    public Friends getSender() {
+    public User getSender() {
         return sender;
     }
 
-    public User getReceiver() {
+    public Friends getReceiver() {
         return receiver;
     }
 
@@ -73,7 +92,10 @@ public class Messaging implements MessagingInterface {
         return isRead;
     }
 
-    // message == sender : content : reciever
+    // SINGLE MESSAGE , message == sender : content : RECEIVER.  singleMessageToString, sendFriendMessage, rewriteSingleMessage
+    // ALL FRIENDS, message == sender: content: All FRIENDS.     allFriendsToString, sendAllFriendsMessage, rewriteAllFriendsMessage
+    // ALL USERS, message == sender: content: ALL USERS.         allUsersToString, sendAllUsersMessage, rewriteAllUsersMessage
+
     public void saveToFile() {
         for (Messaging m : messageHistory) {
             String[] histSplit = m.toString().split(":");
@@ -89,31 +111,81 @@ public class Messaging implements MessagingInterface {
     }
 
 
-    public void sendMessage(Friends sender, User receiver,
-                            String content, String date, Boolean isRead) {
-        if (sender.isBlocked(receiver)) {
+    public void sendMessage(User sender, Friends receiver, String content, String date, Boolean isRead) {
+        if (receiver.isBlocked(sender)) {
             System.out.println("Cannot send message because account you have been blocked.");
         }
 
-        if (!sender.isFriend(receiver)) {
+        if (!receiver.isFriend(sender)) {
             System.out.println("Cannot send message because you are not friends.");
         }
 
         Messaging message = new Messaging(sender, receiver, content, date, isRead);
-
         messageHistory.add(message);
+
+        try(BufferedWriter bfw = new BufferedWriter(new FileWriter(sender.getUsername() + ".txt",true))) {
+            bfw.write(message.toString());
+            bfw.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         System.out.println("Delivered");
     }
 
-    public void deleteMessage(Friends sender, User receiver,
-                              String content, String date, Boolean isRead) {
-        Messaging message = new Messaging(sender, receiver, content, date, isRead);
+    public void sendAllFriendsMessage(User sender, String content, String date, Boolean isRead) {
+        ArrayList<User> AllFriends = Friends.getFriendsList();
+        ArrayList<User> friendsToUser = new ArrayList<>();
+
+        for (User user : AllFriends) {
+            String[] histSplit = user.toString().split(":");
+            String senderFile = histSplit[0] + ".txt";
+            if (histSplit[0].equals(sender.getUsername())) {
+                friendsToUser.add(user);
+            } else {
+                System.out.println("Cannot send message because you have no friends");
+            }
+        }
+
+        Messaging friendsToMessage = new Messaging(sender, content, friendsToUser, date, isRead);
+        messageHistory.add(friendsToMessage);
+
+        try(BufferedWriter bfw = new BufferedWriter(new FileWriter(sender.getUsername() + "AllFriends.txt",true))) {
+            bfw.write(friendsToMessage.toString());
+            bfw.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Delivered");
+    }
+
+    public void sendAllUsersMessage(User sender, String content, String date, Boolean isRead) {
+        ArrayList<User> AllUsers = User.getAllUsers();
+
+        Messaging usersToMessage = new Messaging(sender, content, AllUsers, date, isRead);
+        messageHistory.add(usersToMessage);
+
+        try(BufferedWriter bfw = new BufferedWriter(new FileWriter(sender.getUsername() + "AllUsers.txt",true))) {
+            bfw.write(usersToMessage.toString());
+            bfw.newLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Delivered");
+    }
+
+
+
+    public void deleteMessage(Friends sender, User receiver, String content, String date, Boolean isRead) {
+        Messaging message = new Messaging(receiver, sender, content, date, isRead);
         boolean messageRemoved = false;
         for (int i = 0; i < messageHistory.size(); i++) {
             Messaging messages = messageHistory.get(i);
 
             if (messages.getSender().equals(sender) && messages.getReceiver().equals(receiver)
-                    && messages.getContent().equals(content)) {
+            && messages.getContent().equals(content)) {
                 messageHistory.remove(i);
                 messageRemoved = true;
                 break;
@@ -129,10 +201,8 @@ public class Messaging implements MessagingInterface {
     }
 
     public void report(User sender, User receiver, String content) {
-        try (BufferedWriter bw =
-                     new BufferedWriter(new FileWriter("Report.txt", true))) {
-            String reportEntry =
-                    "Reported User: " + sender.getUsername() + " | Message: \"" + content + "\"" +
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter("Report.txt", true))) {
+            String reportEntry = "Reported User: " + sender.getUsername() + " | Message: \"" + content + "\"" +
                     " | Reported by: " + receiver.getUsername();
             bw.write(reportEntry);
             bw.newLine();
@@ -143,22 +213,27 @@ public class Messaging implements MessagingInterface {
     }
 
     public void deleteConversation(User userOne, User userTwo) {
+        boolean conversationDeleted = false;
         for (int i = 0; i < messageHistory.size(); i++) {
             Messaging message = messageHistory.get(i);
 
-            Friends sender = message.getSender();
+            User sender = message.getSender();
             Friends receiver = message.getReceiver();
-            User senderUser = sender.getUser();
-            User receiverUser = receiver.getUser();
+            User senderUser = sender;
+            Friends receiverUser = receiver;
 
             if ((senderUser.equals(userOne) && receiverUser.equals(userTwo)) ||
                     senderUser.equals(userTwo) && receiverUser.equals(userOne)) {
                 messageHistory.remove(i);
+                conversationDeleted = true;
                 i--; // Fixes index after removing a message from messageHistory
             }
         }
 
-        System.out.println("Conversation between " + userOne.getUsername() + " and " +
-                userTwo.getUsername() + " has been deleted.");
+        if (conversationDeleted) {
+            System.out.println("Conversation between " + userOne.getUsername() + " and " + userTwo.getUsername() + " has been deleted.");
+        } else {
+            System.out.println("No conversation found between " + userOne.getUsername() + " and " + userTwo.getUsername() + ".");
+        }
     }
 }
