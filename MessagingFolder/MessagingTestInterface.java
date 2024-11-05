6f6572982c393fb9cea6057c92016166e81f2d57
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-04 23:46:12 -0500

    Create MessagingTestInterface.java

---
+++ MessagingFolder/MessagingTestInterface.java
@@ -0,0 +1,28 @@
+public interface MessagingTestInterface {
+
+    void testSendMessage();
+
+    void sendAllFriendsMessage();
+
+    void sendAllUsersMessage();
+
+    void testSendMessageToBlockedUser();
+
+    void testDeleteMessage();
+
+    void deleteFriendsMessage();
+
+    void deleteAllMessage();
+
+    void testReport();
+
+    void testSaveToFile();
+
+    void testDeleteConversation();
+
+    void testDeleteAllFriendsConversation();
+
+    void testDeleteAllUsersConversation();
+
+
+}
