686c8d35e0cd747478b5f07aa660a8ac02b0f59b
sabareeshvas29 <vasude12@purdue.edu>
2024-11-15 17:43:27 -0500

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -1,1 +1,39 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+public class Client {
+  
+    private Socket socket;
+    private boolean connected;
+
+    public Client() {
+        this.connected = false;
+    }
+   
+    public boolean connectToServer(String serverAddress, int port) {
+    
+    }
+
+    public void disconnectFromServer() {
+     
+    }
+
+    public boolean isConnected() {
+        return connected;
+    }
+
+    public boolean login(String username, String password) {
+       
+    }
+
+    public void sendRequest(String request) {
+  
+    }
+
+    public String receiveResponse() {
+      
+    }
+
+}
 
