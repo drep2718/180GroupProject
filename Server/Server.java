dba0e733f39a3113e69831920959d90c078f4ba0
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-15 20:12:02 -0500

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -1,1 +1,29 @@
+import java.io.*;
+import java.net.*;
+import java.util.ArrayList;
+
+
+public class Server implements FlagInterface{
+    public static void main(String[] args) throws UnknownHostException, IOException {
+
+        ServerSocket serverSocket = new ServerSocket(2727);
+
+        while (true) {
+            Socket socket = serverSocket.accept();
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            PrintWriter writer = new PrintWriter(socket.getOutputStream());
+
+            while (true) {
+                String message = reader.readLine();
+                if (message.contains(LOGIN))
+                break;
+            }
+            writer.close();
+            reader.close();
+        }
+
+
+    }
+}
 
