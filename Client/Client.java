0687847415d3024ffc67a571f51daaa61de98a2a
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-17 22:06:49 -0500

    Update Client.java

--- Client/Client.java
+++ Client/Client.java
@@ -15,6 +15,7 @@ import java.util.Scanner;
 
 public class Client extends Thread implements FlagInterface {
     private final Server server;
+
     public static void main(String[] args) throws UnknownHostException, IOException, ClassNotFoundException {
         Scanner scan = new Scanner(System.in);
         boolean loggedIn = false;
