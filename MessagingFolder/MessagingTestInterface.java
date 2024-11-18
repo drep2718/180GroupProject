002ea950832e8eebd4338f98ad1d781894d758c9
Santhosh Vaddi <91930012+santhoshv25@users.noreply.github.com>
2024-11-17 21:51:46 -0500

    Update MessagingTestInterface.java

--- MessagingFolder/MessagingTestInterface.java
+++ MessagingFolder/MessagingTestInterface.java
@@ -1,3 +1,11 @@
+/**
+ * Team Project -- MessagingTestInterface Class
+ *
+ * @author Santhosh, Sabareesh, Aiden, Linh, Lab Number: 26043
+ *
+ * @version November 17, 2024
+ *
+ */
 public interface MessagingTestInterface {
 
     void testSendMessage();
