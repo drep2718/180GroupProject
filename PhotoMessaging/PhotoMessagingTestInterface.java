e33f88d3a64a45a096d85d0aa99f5430ca2f87b5
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-20 23:20:03 -0500

    Create PhotoMessagingTestInterface.java

---
+++ PhotoMessaging/PhotoMessagingTestInterface.java
@@ -0,0 +1,27 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Team Project -- PhotomessagingTest Interface
+ *
+ * @author Santhosh, Sabareesh, Aiden, Linh, Lab Number: 26043
+ * @version November 17, 2024
+ */
+
+
+public interface PhotoMessagingTestInterface {
+    void testSendAllFriendsPhotoMessage();
+
+    void testSendAllUsersPhotoMessage();
+
+    void testDeletePhotoMessage();
+
+    void testDeleteFriendsPhotoMessage();
+
+    void testDeleteAllPhotoMessage();
+
+    void testDeleteConversation();
+    
+    void testDeleteAllFriendsConversation();
+
+    void testDeleteAllUsersConversation();
+}
