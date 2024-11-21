d2a44af2388454013ec5fdfe90d21e888b777909
Aiden Drepaniotis <140779311+drep2718@users.noreply.github.com>
2024-11-20 23:12:49 -0500

    Update FriendsTestInterface.java

--- FriendsFolder/FriendsTestInterface.java
+++ FriendsFolder/FriendsTestInterface.java
@@ -1,1 +1,33 @@
 
+/**
+ * Team Project -- FriendsTest Interface
+ *
+ * @author Santhosh, Sabareesh, Aiden, Linh, Lab Number: 26043
+ * @version November 17, 2024
+ */
+
+public interface FriendsTestInterface {
+
+    void addFriend(User user);
+
+    void addFriend();
+
+    void removeFriend();
+
+    void blockUser();
+
+    void unblockUser();
+
+    void isFriend();
+
+    void isBlocked();
+
+    void saveFriendsToFile();
+
+    void saveBlockedToFile();
+
+    void rewriteFriends();
+
+    void rewriteBlocked();
+
+}
