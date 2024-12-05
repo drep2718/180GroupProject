9d7a45b271b9fd9c8ded6731087b52fd2ec48fc5
Drep2718 <aidendrep@icloud.com>
2024-12-05 15:53:48 -0500

    test line in client

--- GUIDesign/ComplexGUI.java
+++ GUIDesign/ComplexGUI.java
@@ -1,10 +1,12 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class ComplexGUI {
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> new WelcomeScreen().setVisible(true));
+      SwingUtilities.invokeLater(() -> new WelcomeScreen().setVisible(true));
     }
 }
 
@@ -44,9 +46,141 @@ class WelcomeScreen extends JFrame {
         continueButton.setFont(new Font("Arial", Font.PLAIN, 16));
         continueButton.addActionListener(e -> {
             new MainGUI().setVisible(true);
+            SwingUtilities.invokeLater(() -> new mainMenu().setVisible(true));
             dispose();
         });
         bottomBar.add(continueButton, BorderLayout.CENTER);
+        add(bottomBar, BorderLayout.SOUTH);
+    }
+
+
+    class MainGUI extends JFrame {
+
+    }
+}
+
+
+class mainMenu extends JFrame {
+    public mainMenu() {
+        setTitle("Main Menu");
+        setSize(1000, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        JLabel welcomeLabel = new JLabel("Welcome to The Main Menu", SwingConstants.CENTER);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 40));
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(welcomeLabel, BorderLayout.CENTER);
+        add(centerPanel, BorderLayout.CENTER);
+
+        JPanel topBar = new JPanel();
+        topBar.setBackground(Color.BLACK);
+        topBar.setPreferredSize(new Dimension(getWidth(), 100));
+        add(topBar, BorderLayout.NORTH);
+
+        JPanel bottomBar = new JPanel();
+        bottomBar.setBackground(new Color(229, 194, 31));
+        bottomBar.setPreferredSize(new Dimension(getWidth(), 400));
+        bottomBar.setLayout(new GridLayout(3, 1, 10, 10));
+
+
+        JButton loginButton = new JButton("Login");
+        loginButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        loginButton.addActionListener(e -> {
+            new MainGUI().setVisible(true);
+            SwingUtilities.invokeLater(() -> new LoginMenu().setVisible(true));
+            dispose();
+        });
+
+        JButton createButton = new JButton("Create User");
+        createButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        createButton.addActionListener(e -> {
+            new MainGUI().setVisible(true);
+            dispose();
+        });
+
+        JButton exitButton = new JButton("Exit");
+        exitButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        exitButton.addActionListener(e -> {
+            new MainGUI().setVisible(true);
+            dispose();
+        });
+
+
+        bottomBar.add(loginButton);
+        bottomBar.add(createButton);
+        bottomBar.add(exitButton);
+        add(bottomBar, BorderLayout.SOUTH);
+    }
+
+
+    class MainGUI extends JFrame {
+
+    }
+}
+
+class LoginMenu extends JFrame {
+    public LoginMenu() {
+        setTitle("Login Menu");
+        setSize(1000, 700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setLayout(new BorderLayout());
+
+        JLabel welcomeLabel = new JLabel("Welcome to The Login Menu", SwingConstants.CENTER);
+        welcomeLabel.setFont(new Font("Arial", Font.BOLD, 40));
+
+        JPanel centerPanel = new JPanel(new BorderLayout());
+        centerPanel.add(welcomeLabel, BorderLayout.CENTER);
+//        add(centerPanel, BorderLayout.CENTER);
+
+        JPanel topBar = new JPanel();
+        topBar.setBackground(Color.BLACK);
+        topBar.setPreferredSize(new Dimension(getWidth(), 100));
+        add(topBar, BorderLayout.NORTH);
+
+        JPanel bottomBar = new JPanel();
+        bottomBar.setBackground(new Color(229, 194, 31));
+        bottomBar.setPreferredSize(new Dimension(getWidth(), 100));
+        bottomBar.setLayout(new BorderLayout());
+
+        JPanel central = new JPanel(new GridLayout(2,2));
+        central.setBackground(new Color(229, 194, 31));
+
+        JLabel usernameLabel = new JLabel("Username:", SwingConstants.CENTER);
+        usernameLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        central.add(usernameLabel);
+
+        JTextField username = new JTextField();
+        username.setFont(new Font("Arial", Font.PLAIN, 30));
+        central.add(username);
+
+        JLabel passwordLabel = new JLabel("Password:", SwingConstants.CENTER);
+        passwordLabel.setFont(new Font("Arial", Font.BOLD, 30));
+        central.add(passwordLabel);
+
+        JTextField password = new JTextField();
+        password.setFont(new Font("Arial", Font.PLAIN, 30));
+        central.add(password);
+
+        centerPanel.add(central, BorderLayout.SOUTH);
+        add(centerPanel, BorderLayout.CENTER);
+
+        JButton enterButton = new JButton("Enter");
+        enterButton.setFont(new Font("Arial", Font.PLAIN, 30));
+        enterButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                String usernameText = username.getText();
+                String passwordText = password.getText();
+                System.out.println(usernameText);
+                System.out.println(passwordText);
+                dispose();
+            }
+        });
+
+        bottomBar.add(enterButton, BorderLayout.CENTER);
         add(bottomBar, BorderLayout.SOUTH);
     }
 
