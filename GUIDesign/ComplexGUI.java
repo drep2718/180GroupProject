f3cd65136064038244bdf9604f5e2fe99dd7e889
Linh Dang <dang92@purdue.edu>
2024-12-10 00:15:03 -0500

    Update ComplexGUI.java

--- GUIDesign/ComplexGUI.java
+++ GUIDesign/ComplexGUI.java
@@ -1244,7 +1244,7 @@ class deleteMenu extends JFrame {
                 singleFriendMessagePanel.add(new JLabel("Who would you like to delete the message from?"));
                 singleFriendMessagePanel.add(friendDropdown);
 
-                Friends selectedFriend = (Friends) friendDropdown.getSelectedItem();
+                String selectedFriend = (String) friendDropdown.getSelectedItem();
 
                 int result = JOptionPane.showConfirmDialog(deleteMenu.this,
                         singleFriendMessagePanel, "Select Friend", JOptionPane.OK_CANCEL_OPTION);
@@ -1265,7 +1265,7 @@ class deleteMenu extends JFrame {
                 int resultMessage = JOptionPane.showConfirmDialog(deleteMenu.this,
                         singleMessagePanel, "Select Message", JOptionPane.OK_CANCEL_OPTION);
                 if (resultMessage == JOptionPane.OK_OPTION) {
-                    messaging.deleteMessage(user, selectedFriend, selectedMessage, date, isRead);
+                    messaging.deleteMessage(user, receiver, selectedMessage, date, isRead);
                     JOptionPane.showMessageDialog(deleteMenu.this,
                             "Message deleted.");
                     updateSingleFriendMessage();
